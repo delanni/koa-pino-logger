93fa7003c5b83ae7ed72ed84e6f8a764b416e6ac
Jonathan Stewmon <jstewmon@gmail.com>
2019-04-03 15:31:39 -0500

    update pino-http to ^4.1.0
    - remove explicit log statement in favor of providing res.err, which is
      recognized by pino-http
    - added pino ^5.12.0 as a devDependency to support extreme mode
      benchmark

--- benchmarks/pino-extreme-server.js
+++ benchmarks/pino-extreme-server.js
@@ -1,10 +1,11 @@
 'use strict'
 
 var Koa = require('koa')
+var pino = require('pino')
 var logger = require('../')
 
 var app = new Koa()
-app.use(logger({extreme: true}))
+app.use(logger(pino.extreme()))
 
 app.use((ctx) => {
   ctx.body = 'hello world'
--- logger.js
+++ logger.js
@@ -6,22 +6,18 @@ module.exports = logger
 
 function logger (opts, stream) {
   var wrap = pinoHttp(opts, stream)
+
   function pino (ctx, next) {
     wrap(ctx.req, ctx.res)
     ctx.log = ctx.request.log = ctx.response.log = ctx.req.log
     return next().catch(function (e) {
-      ctx.log.error({
-        res: ctx.res,
-        err: {
-          type: e.constructor.name,
-          message: e.message,
-          stack: e.stack
-        },
-        responseTime: ctx.res.responseTime
-      }, 'request errored')
+      if (!('err' in ctx.res)) {
+        ctx.res.err = e
+      }
       throw e
     })
   }
+
   pino.logger = wrap.logger
   return pino
 }
